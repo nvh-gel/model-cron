fbfdacd548e85ac71841ed15bef046f543fdfaf0
nvh-gel <nvhien2703@outlook.com>
2023-04-21 22:23:34 +0700

    update git ignore and version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.6"
+    id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "com.eden.gallery"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
