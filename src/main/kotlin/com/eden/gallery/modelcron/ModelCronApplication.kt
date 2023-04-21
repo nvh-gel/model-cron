5d1934857c6d417c476f947696918b32ed01e891
nvh-gel <nvhien2703@outlook.com>
2023-04-21 22:22:13 +0700

    Initial commit

---
+++ src/main/kotlin/com/eden/gallery/modelcron/ModelCronApplication.kt
@@ -0,0 +1,11 @@
+package com.eden.gallery.modelcron
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModelCronApplication
+
+fun main(args: Array<String>) {
+    runApplication<ModelCronApplication>(*args)
+}
---
+++ src/test/kotlin/com/eden/gallery/modelcron/ModelCronApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eden.gallery.modelcron
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ModelCronApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
