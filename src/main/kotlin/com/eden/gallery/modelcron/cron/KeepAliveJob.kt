cb614ff72eb020b890ee3d2587d0bb883949d7de
nvh-gel <nvhien2703@outlook.com>
2023-05-06 09:39:13 +0700

    GAL-84 implement re-crawl job

--- src/main/kotlin/com/eden/gallery/modelcron/cron/KeepAliveJob.kt
+++ src/main/kotlin/com/eden/gallery/modelcron/cron/KeepAliveJob.kt
@@ -1,5 +1,6 @@
 package com.eden.gallery.modelcron.cron
 
+import com.eden.gallery.modelcron.service.ConfigService
 import org.apache.logging.log4j.kotlin.Logging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -17,7 +18,8 @@ import java.util.concurrent.TimeUnit
 )
 @Component
 class KeepAliveJob(
-    @Autowired val template: RestTemplate
+    @Autowired val template: RestTemplate,
+    @Autowired val configService: ConfigService,
 ) : Logging {
 
     /**
@@ -25,9 +27,12 @@ class KeepAliveJob(
      */
     @Scheduled(fixedRate = 14, timeUnit = TimeUnit.MINUTES)
     fun keepStagingGalleryApiAlive() {
-
-        template.getForEntity("https://model-gallery-api-staging.onrender.com/healthz", String::class.java)
-        logger.info("job keep gallery dev api success at: ${LocalDateTime.now()}")
+        val now = LocalDateTime.now()
+        val enable: Boolean = configService.findConfig("KEEP_ALIVE_STAGING")?.value.toBoolean()
+        if (enable && now.hour < 17) {
+            template.getForEntity("https://model-gallery-api-staging.onrender.com/healthz", String::class.java)
+            logger.info("job keep gallery dev api success at: $now")
+        }
     }
 
     /**
@@ -35,18 +40,19 @@ class KeepAliveJob(
      */
     @Scheduled(fixedRate = 14, timeUnit = TimeUnit.MINUTES)
     fun keepProdGalleryApiAlive() {
-
-        template.getForEntity("https://model-gallery-api.onrender.com/healthz", String::class.java)
-        logger.info("job keep gallery dev api success at: ${LocalDateTime.now()}")
+        val now = LocalDateTime.now()
+        val enable: Boolean = configService.findConfig("KEEP_ALIVE_PROD")?.value.toBoolean()
+        if (enable && now.hour < 17) {
+            template.getForEntity("https://model-gallery-api.onrender.com/healthz", String::class.java)
+            logger.info("job keep gallery dev api success at: $now")
+        }
     }
-
 
     /**
      * Job to keep staging cron alive.
      */
     @Scheduled(fixedRate = 14, timeUnit = TimeUnit.MINUTES)
     fun keepCronAlive() {
-
         template.getForEntity("https://model-cron.onrender.com/healthz", String::class.java)
         logger.info("job keep cron dev success at: ${LocalDateTime.now()}")
     }
